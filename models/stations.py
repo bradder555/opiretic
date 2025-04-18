084a1bf5a2bdbde6a787c06710ae978ef794ba7c
brad <bradder555@users.noreply.github.com>
2025-04-18 19:25:40 +0800

    adds default method, and overrides to station

--- models/stations.py
+++ models/stations.py
@@ -6,8 +6,19 @@ class Station:
         self, 
         station_id: int, 
         programs: list[Program],
-        override: Override
+        override: Override | None,
+        weather_override: Override | None 
     ):
         self.station_id = station_id 
-        self.conditions = conditions
+        self.programs = programs
         self.override = override
+        self.weather_override = weather_override
+
+    @classmethod
+    def default(cls):
+        return cls(
+            1,
+            programs = [Program.default()],
+            override = None,
+            weather_override = None
+        )
