e62adc931c764c37319ece5b7f82bdd6251fd0a8
brad <bradder555@users.noreply.github.com>
2025-04-18 19:26:33 +0800

    wip: start of configuration code

--- main.py
+++ main.py
@@ -1,9 +1,46 @@
 from typing import Union
+from contextlib import asynccontextmanager
+from models import Station
+import yaml
+
+import os, sys 
 
 from fastapi import FastAPI
 
-app = FastAPI()
+def get_config_context():
+    config = {}
+    try:
+        with open("config.yaml", "w") as conf:
+            config = yaml.parse(conf)
+    except:
+        config["stations"] = []
+        for i in range(6):
+            s = Station.default()
+            s.station_id = i
+            config["stations"].append(s)
+        with open("config.yaml", "w") as conf:
+            yaml.dump(config, conf)
 
+    def read_config():
+        return config
+
+    def write_config():
+        pass
+    
+    return {
+        "read": read_config,
+        "write": write_config
+    }
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Load the ML model
+    ml_models["answer_to_everything"] = fake_answer_to_everything_ml_model
+    yield
+    # Clean up the ML models and release the resources
+    ml_models.clear()
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def read_root():
