d0096e0ed2649bf2982c67b6dae4639121cdb6b4
Brad <17851543+bradder555@users.noreply.github.com>
2025-04-19 00:18:09 +0800

    adds 'status' logic to station

--- models/stations.py
+++ models/stations.py
@@ -32,6 +32,7 @@ class Station:
         station_id: int, 
         programs: list[Program],
         override: Override | None,
+        weather_override: Override | None ,
         description: str = ""
     ):
         self.station_id = station_id 
@@ -48,3 +49,24 @@ class Station:
             override = None,
             weather_override = None
         )
+
+    def status(self, datetime : Optional[datetime] = None):
+        if datetime is None:
+            datetime = datetime.now()
+
+        override_active, override_type = None, None if self.override is None else self.override.applies(datetime)
+        weather_active, weather_type = None, None if self.weather_override is None else self.weather_override.applies(datetime)
+
+        summary = StationSummaryModel(
+            station_id = self.station_id,
+            description = self.description if self.description else "",
+            override_active = override_active,
+            override_type = override_type,
+            weather_override = weather_active,
+            weather_override_type = weather_type,
+            program_states = [
+                x.run(datetime) for x in self.programs
+            ]
+        )
+        return summary 
+        
